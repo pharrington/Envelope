73007fed22a9288e6e9654113cfd3076ed04f292
Paul Harrington <pharrington@badenoughdu.de>
2009-06-22 19:12:26 -0400

    cleaning up temporary files

--- attachments/56/03/xeno@badenoughdu.de/why.rb
+++
@@ -1,8 +0,0 @@
-require 'rbconfig'
-bui = /^bui(.{2})$/
-$stdout << "#{{}.class}"[0,1] <<
- ("#{{}.methods}"[/c(\w{4})c/] && $1.reverse) <<
- (([0]*2).inspect[2,2]) <<
- Config::CONFIG.keys.grep(bui).first.gsub(bui,
-   "#{Kernel.methods.grep(/^th/)[0][2,3].reverse}\\1") <<
- ObjectSpace._id2ref(338)
